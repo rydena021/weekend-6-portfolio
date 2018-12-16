63b781dcd52b44dd00816bbbec2bb0b1fa43f22c
Anders Ryden <rydena021@gmail.com>
2018-12-16 17:20:28 -0600

    setup pool and connect to db

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   // Renders the entire app on the DOM
+  handleClick = () => {
+    axios.get('api/project').then( response => {
+      console.log(response);
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <p>Empty Page</p>
+        <button onClick={this.handleClick}>test</button>
       </div>
     );
   }
